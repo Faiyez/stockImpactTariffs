de758d10547c9609db28f871b305228ea2b6722b
Faiyez Noor <noorfaiyez@gmail.com>
2025-07-19 14:46:25 -0230

    Evaluated Tickers

--- stockData.py
+++ stockData.py
@@ -5,7 +5,17 @@ import kagglehub
 path = kagglehub.dataset_download("nelgiriyewithana/world-stock-prices-daily-updating")
 
 df = pd.read_csv("World-Stock-Prices-Dataset.csv")
-df.head()
-tickers = ['AAPL','AMZN','MSFT','NVDA']
+
+# Convert 'Date' to datetime right after reading
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+print("After conversion:", df['Date'].dtype)
+
+# Drop rows where 'Date' couldn't be parsed
+df = df.dropna(subset=['Date'])
+
+tickers = ['AAPL','AMZN','MSFT','NVDA','BABA','TCEHY','JD','BYDDY','NIO']
 df = df[df['Ticker'].isin(tickers)]
-df.head()
+
+df = df[['Date','Open','Close','Brand_Name','Ticker']]
+df['Day'] = df['Date'].dt.day
+print(df.dtypes)
